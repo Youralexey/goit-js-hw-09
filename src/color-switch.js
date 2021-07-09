8c3c88ff09f9ed6203b0a69c4f4e777d860aa72f
Youralexey <johndoe@example.com>
2021-07-09 23:16:54 +0300

    сделал color-switch

---
+++ src/color-switch.js
@@ -0,0 +1,25 @@
+
+let intervalId = null;
+
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+
+startBtn = document.addEventListener('click', onClickBtnStart);
+stopBtn = document.addEventListener('click', onClickBtnStop);
+
+function onClickBtnStart() {
+  startBtn.disabled = true;
+  intervalId = setInterval(() => {
+      const selectedColor = getRandomHexColor;
+    document.body.style.backgroundColor = selectedColor;
+  },1000)
+}
+
+function onClickBtnStop() {
+  startBtn.disabled = false;
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
